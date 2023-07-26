577ab10e556b13647210169ddd384706edc9c25c
jastpl2153 <jastpl2153@mail.ru>
2023-07-26 09:27:06 +0800

    Added an interface for the application

---
+++ src/main/java/com/example/recipebook/Main.java
@@ -0,0 +1,25 @@
+package com.example.recipebook;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/recipebook/MainWindow.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setTitle("Книга рецептов");
+        stage.setScene(scene);
+
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
