88e1dab31568379edbfb97d54502b2df5b80037d
jastpl2153 <jastpl2153@mail.ru>
2023-07-28 00:55:54 +0800

    Added implementation of recipe viewing

--- src/main/java/com/example/recipebook/controller/ControllerMainWindow.java
+++ src/main/java/com/example/recipebook/controller/ControllerMainWindow.java
@@ -1,19 +1,13 @@
 package com.example.recipebook.controller;
 
-import com.example.recipebook.model.Recipe;
 import com.example.recipebook.model.RecipeDAO;
+import com.example.recipebook.model.RecipeDataModel;
 import com.example.recipebook.model.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.sql.SQLException;
-import java.util.List;
 
 public class ControllerMainWindow {
     private RecipeDAO recipeDAO = new RecipeDAO();
@@ -25,29 +19,16 @@ public class ControllerMainWindow {
     }
 
     @FXML
-    void menuEnter(ActionEvent event) throws SQLException {
+    void menuEnter(ActionEvent event) {
         Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         SceneSwitcher.switchScene("/com/example/recipebook/MenuWindow.fxml", currentStage);
 
-        ControllerMenuWindow con = SceneSwitcher.getController();
+        ControllerMenuWindow con = (ControllerMenuWindow) SceneSwitcher.getController();
         String type = ((Button) event.getSource()).getText();
-        List<Recipe> recipes = recipeDAO.getRecipe(type);
 
-        updateRecipeButtons(con, recipes);
-    }
+        RecipeDataModel.getInstance().setRecipes(recipeDAO.getRecipe(type));
 
-    private void updateRecipeButtons(ControllerMenuWindow controller, List<Recipe> recipes) {
-        controller.getBox().getChildren().clear();
-
-        controller.getBox().getChildren().addAll(recipes.stream()
-                .map(recipe -> new Button(recipe.getTitle()))
-                .toList());
-
-        controller.getBox().getChildren().forEach(node -> {
-            VBox.setMargin(node, new Insets(10, 15, 0, 15));
-            ((Region) node).setMaxWidth(Double.MAX_VALUE);
-            node.setStyle("-fx-background-color: #b6eed8");
-        });
+        con.updateRecipeButtons(RecipeDataModel.getInstance().getRecipes());
     }
 
     @FXML
