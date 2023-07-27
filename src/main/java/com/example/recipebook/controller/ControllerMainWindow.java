63985f0de4637fe6aaad8e7b340188562d21cd45
jastpl2153 <jastpl2153@mail.ru>
2023-07-27 12:19:48 +0800

    Added database and connection to it

--- src/main/java/com/example/recipebook/controller/ControllerMainWindow.java
+++ src/main/java/com/example/recipebook/controller/ControllerMainWindow.java
@@ -3,13 +3,8 @@ package com.example.recipebook.controller;
 import com.example.recipebook.model.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class ControllerMainWindow {
     @FXML
---
+++ src/main/java/com/example/recipebook/model/Recipe.java
@@ -0,0 +1,75 @@
+package com.example.recipebook.model;
+
+
+public class Recipe {
+    private String title;
+
+    private String typeOfMeal;
+
+    private String typeOfFood;
+
+    private String ingredients;
+
+    private String instructions;
+
+    public Recipe(String title, String typeOfMeal, String typeOfFood, String ingredients, String instructions) {
+        this.title = title;
+        this.typeOfMeal = typeOfMeal;
+        this.typeOfFood = typeOfFood;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+    }
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTypeOfMeal() {
+        return typeOfMeal;
+    }
+
+    public void setTypeOfMeal(String typeOfMeal) {
+        this.typeOfMeal = typeOfMeal;
+    }
+
+    public String getTypeOfFood() {
+        return typeOfFood;
+    }
+
+    public void setTypeOfFood(String typeOfFood) {
+        this.typeOfFood = typeOfFood;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    // Override toString() method for easier debugging and printing
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Title: ").append(title).append("\n");
+        sb.append("TypeOfMeal: ").append(typeOfMeal).append("\n");
+        sb.append("TypeOfFood: ").append(typeOfFood).append("\n");
+        sb.append("Ingredients: ").append(ingredients).append("\n");
+        sb.append("Instructions: ").append(instructions).append("\n");
+        return sb.toString();
+    }
+}
