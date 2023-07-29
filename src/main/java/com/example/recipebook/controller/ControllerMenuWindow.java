e04da5c554922dfa7114d2e7e7dc7db92e3808d0
jastpl2153 <jastpl2153@mail.ru>
2023-07-29 19:16:26 +0800

    Added the recipe sorting function

--- src/main/java/com/example/recipebook/controller/ControllerMenuWindow.java
+++ src/main/java/com/example/recipebook/controller/ControllerMenuWindow.java
@@ -1,17 +1,17 @@
 package com.example.recipebook.controller;
 
 import com.example.recipebook.model.Recipe;
+import com.example.recipebook.model.RecipeDataModel;
 import com.example.recipebook.model.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ControllerMenuWindow {
     @FXML
@@ -51,6 +51,21 @@ public class ControllerMenuWindow {
             recipeButton.setMaxWidth(Double.MAX_VALUE);
             recipeButton.setStyle("-fx-background-color: #b6eed8");
         });
+    }
+
+    @FXML
+    void sort(ActionEvent event) {
+        MenuItem menuItem = (MenuItem) event.getSource();
+        if (menuItem.getText().equals("А - Я")) {
+            sortRecipes((recipe1, recipe2) -> recipe1.getTitle().compareToIgnoreCase(recipe2.getTitle()));
+        } else if (menuItem.getText().equals("Я - А")) {
+            sortRecipes((recipe1, recipe2) -> recipe2.getTitle().compareToIgnoreCase(recipe1.getTitle()));
+        }
+    }
+
+    private void sortRecipes(Comparator<Recipe> comparator) {
+        RecipeDataModel.getInstance().getRecipes().sort(comparator);
+        updateRecipeButtons(RecipeDataModel.getInstance().getRecipes());
     }
 
     private Stage getStage(){
